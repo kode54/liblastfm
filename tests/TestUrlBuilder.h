e23862b4df4a052845a1f050014073a62425e06c
Leandro Nini <drfiemost@users.noreply.github.com>
2021-11-02 08:35:25 +0100

    test: replace test with one using a different special character

--- tests/TestUrlBuilder.h
+++ tests/TestUrlBuilder.h
@@ -52,7 +52,7 @@ private slots:
 
         QTest::newRow( "ascii" ) << "Metallica" << "Metallica";
         QTest::newRow( "ascii alphanumeric" ) << "Apollo 440" << "Apollo+440";
-        QTest::newRow( "ascii with symbols" ) << "some track [original version]" << "some+track+%5Boriginal+version%5D";
+        QTest::newRow( "ascii with symbols" ) << "Would?" << "Would%3F";
         QTest::newRow( "ascii with last.fm-special symbols" ) << "Survivalism [Revision #1]" << "Survivalism+%5BRevision+%231%5D";
     }
 
